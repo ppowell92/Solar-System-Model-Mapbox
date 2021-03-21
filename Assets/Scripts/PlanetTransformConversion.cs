818e0b5d3a169b78780143c2d99891f2f58f2d25
Phillip Powell <pmpowell92@gmail.com>
2021-03-21 10:15:05 -0600

    Updated map location

---
+++ Assets/Scripts/PlanetTransformConversion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using Mapbox.Unity.Utilities;
+
+public class PlanetTransformConversion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
